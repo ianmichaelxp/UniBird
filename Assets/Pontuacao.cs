332879231fd0c2cca3564bd1c393120399c3e0a1
Ian Michael <ianmichael.xp@gmail.com>
2020-10-02 03:24:20 -0300

    pontuação

---
+++ Assets/Pontuacao.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pontuacao : MonoBehaviour {
+   [SerializeField]
+    private Text textoPontuacao;
+    private int pontos;
+   
+
+    public void AdicionarPontos()
+    {
+        this.pontos++;
+        this.textoPontuacao.text = this.pontos.ToString();
+    }
+
+    public void Reiniciar()
+    {
+        this.pontos = 0;
+        this.textoPontuacao.text = this.pontos.ToString();
+    }
+}
